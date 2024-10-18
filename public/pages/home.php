ff6a8437cbe400acdb3a19002992b63bbc4a72a6
raincuhh <146904648+raincuhh@users.noreply.github.com>
2024-10-18 13:27:54 +0200

    home page more stuff

--- public/pages/home.php
+++ public/pages/home.php
@@ -4,13 +4,65 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="La Tavola del Sapore | home" />
   <title>La Tavola del Sapore | Home</title>
+
+  <script defer>
+    let date_now = new Date();
+    let week_day_unfm = date_now.getDay();
+    let week_day_fm = (number) => {
+      switch (number) {
+        case 1:
+          return "Mon";
+        case 2:
+          return "Tue";
+        case 3:
+          return "Wed";
+        case 4:
+          return "Thu";
+        case 5:
+          return "Fri";
+        case 6:
+          return "Sat";
+        case 7:
+          return "Sun";
+        default:
+          return "Unknown";
+      }
+    };
+    let date = date_now.getDate();
+    let month = date_now.getMonth();
+    let year = date_now.getFullYear();
+
+    const formatted = week_day_fm(week_day_unfm) + " " + date + " " + month + " " + year;
+    console.log(week_day_unfm); // test
+    console.log("Date now: " + formatted);
+  </script>
+
+  <link rel="stylesheet" href="../themes/variables.css">
+  <link rel="stylesheet" href="home.css">
 </head>
 
 <body>
   <div id="app">
     <main id="main">
-      wadwad
+      <nav class="nav">
+        <div class="nav_wrapper">
+          <div class="nav_header">
+            <p>
+              <span>La Tavola del Sapore</span>
+
+              <span>/ Home</span>
+            </p>
+          </div>
+          <div class="nav_cat">
+            <div class="nav_cat_list">
+              <div class="nav_cat_item"></div>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div class="page"></div>
     </main>
   </div>
   <div id="load" style="pointer-events: all">
