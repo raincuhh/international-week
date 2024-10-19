d236885d92744a6af4cb5dc4facda10216cdbff2
Filip Ryan <146904648+raincuhh@users.noreply.github.com>
2024-10-19 05:59:44 -0700

    added favicon

--- public/pages/home.php
+++ public/pages/home.php
@@ -6,7 +6,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="Il Sapore dell'Italia | home" />
   <title>Il Sapore dell'Italia | Home</title>
-
+  <link rel="apple-touch-icon" sizes="180x180" href="../assets/icons/favicon/apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="../assets/icons/favicon/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="../assets/icons/favicon/favicon-16x16.png" />
+  <link rel="manifest" href="../assets/icons/favicon/site.webmanifest" />
   <script defer>
     let date_now = new Date();
     let week_day_unfm = date_now.getDay();
